45f3b9732c0344aa930b95ce4593e4caa0457d83
spborder <spborder@buffalo.edu>
2022-10-31 17:24:18 -0400

    Initial commit

---
+++ CellKappaVis.py
@@ -0,0 +1,32 @@
+"""
+
+Codes to calculate Cohen's Kappa scores between different combinations of annotators
+
+Some basic visualization implementation
+
+
+"""
+
+import os
+import sys
+import numpy as np
+
+from PIL import Image
+
+from dash import dcc, ctx
+
+from dash_extensions.enrich import DashProxy, html, Input, Output, MultiplexerTransform
+
+
+
+
+
+
+
+
+
+
+
+
+
+
