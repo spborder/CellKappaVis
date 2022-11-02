8c74b3849f3e138e58b048da4714c5354d390169
spborder <spborder@buffalo.edu>
2022-11-02 14:50:41 -0400

    Allowing for comparison to All for imgs and raters

--- CellKappaVis.py
+++ CellKappaVis.py
@@ -69,7 +69,7 @@ for a in annotators:
 
         annotator_img_dict[a][img_name]['All_Cell_Types'] = pd.concat([annotator_img_dict[a][img_name][i] for i in annotation_codes],ignore_index=True)
 
-imgs_annotated = np.unique(imgs_annotated)
+imgs_annotated = np.unique(imgs_annotated).tolist()
 initial_img = annotator_img_dict[annotators[0]][imgs_annotated[0]]['Image']
 
 initial_figure = make_subplots(rows=1,cols=2,subplot_titles = (f'Annotator 1: {annotators[0]}',f'Annotator 2: {annotators[0]}'))
@@ -85,14 +85,6 @@ for i, figure in enumerate(imgs_to_include):
     else:
         initial_figure.append_trace(figure,row=1,col=i+1)
 
-
-# In this case, the centroids are stored in a dict format so 
-centroids_1 = annotator_img_dict[annotators[0]][imgs_annotated[0]]['All_Cell_Types']
-centroids_2 = annotator_img_dict[annotators[0]][imgs_annotated[0]]['All_Cell_Types']
-
-# KDTree search
-tree = cKDTree(centroids_1.values[:,0:2])
-distances, idxes = tree.query(centroids_2.values[:,0:2])
 
 
 main_layout = html.Div([
@@ -111,19 +103,19 @@ main_layout = html.Div([
         ]),
         html.Div([
             dcc.Dropdown(
-                imgs_annotated,
+                imgs_annotated+['All'],
                 imgs_annotated[0],
                 id = 'image-name'
             ),
             html.Br(),
             dcc.Dropdown(
-                annotators,
+                annotators+['All'],
                 annotators[0],
                 id = 'rater-1'
             ),
             html.Br(),
             dcc.Dropdown(
-                annotators,
+                annotators+['All'],
                 annotators[0],
                 id = 'rater-2'
             )
@@ -157,33 +149,130 @@ class CellKappaVis:
 
     def update_raters_image(self,rater_1,rater_2,img_name):
         
-        # Images from both raters
-        image_1 = self.annotator_img_dict[rater_1][img_name]['Image']
-        image_2 = self.annotator_img_dict[rater_2][img_name]['Image']
 
-        new_img_figure = self.make_img_subplots(image_1,image_2, rater_1, rater_2)
-        new_conf_mat, new_kappa = self.generate_new_confusion_matrix(rater_1,rater_2,img_name)
+        if not 'All' in [rater_1,rater_2]:
+            # Comparing two specific raters
+            new_conf_mat, new_kappa = self.generate_new_confusion_matrix(rater_1,rater_2,img_name)
 
+            if not img_name=='All':
+                # One specific image from both raters
+                if img_name in self.annotator_img_dict[rater_1] and img_name in self.annotator_img_dict[rater_2]:
+                    image_1 = self.annotator_img_dict[rater_1][img_name]['Image']
+                    image_2 = self.annotator_img_dict[rater_2][img_name]['Image']
+
+                    new_img_figure = self.make_img_subplots([image_1,image_2], [rater_1, rater_2])
+                else:
+                    try:
+                        image = self.annotator_img_dict[rater_1][img_name]['Image']
+                        new_img_figure = go.Figure(px.imshow(image).data[0])
+                        new_img_figure.update_layout(title=f'Only annotated by {rater_1}')
+                    except:
+                        image = self.annotator_img_dict[rater_2][img_name]['Image']
+                        new_img_figure = go.Figure(px.imshow(image).data[0])
+                        new_img_figure.update_layout(title=f'Only annotated by {rater_2}')   
+            else:
+                # All images from both raters
+                overlap_image_list = list(set(list(self.annotator_img_dict[rater_1].keys()) & set(list(self.annotator_img_dict[rater_2].keys()))))
+
+                image_1 = self.annotator_img_dict[rater_1][overlap_image_list[0]]['Image']
+                image_2 = self.annotator_img_dict[rater_2][overlap_image_list[0]]['Image']
+
+                new_img_figure = self.make_img_subplots([image_1,image_2],[rater_1,rater_2])
+
+                conf_mat_list = []
+                kappa_list = []
+                for image in overlap_image_list:
+                    new_conf_mat, new_kappa = self.generate_new_confusion_matrix(rater_1,rater_2,image)
+                    conf_mat_list.append(new_conf_mat)
+                    kappa_list.append(new_kappa)
+
+                new_conf_mat = np.sum(np.array(conf_mat_list),axis=0)
+                new_kappa = np.mean(kappa_list)
+
+        else:
+            # Comparing all raters to one or all raters
+            if rater_1=='All' and not rater_2 == 'All':
+                # Comparing one rater to all
+                raters_list_1 = [i for i in self.annotator_img_dict if not i == rater_2]
+                raters_list_2 = [rater_2]
+            elif not rater_1=='All' and rater_2=='All':
+                # Comparing one rater to all
+                raters_list_1 = [rater_1]
+                raters_list_2 = [i for i in self.annotator_img_dict if not i == rater_1]
+            else:
+                # Comparing all to all
+                raters_list_1 = [i for i in self.annotator_img_dict]
+                raters_list_2 = [i for i in self.annotator_img_dict]
+
+            # Show images for every rater no matter what but show a specific image for each rater if All is not 
+            # selected for image
+            if not img_name == 'All':
+                image_list = [self.annotator_img_dict[r][img_name]['Image'] if img_name in self.annotator_img_dict[r] else np.zeros((200,200)) for r in self.annotator_img_dict]
+
+                new_img_figure = self.make_img_subplots(image_list,list(self.annotator_img_dict.keys()))
+
+                conf_mat_list = []
+                kappa_list = []
+                for r1 in raters_list_1:
+                    for r2 in raters_list_2:
+                        # Not comparing a rater to themselves
+                        if not r1==r2:
+                            new_conf_mat, new_kappa = self.generate_new_confusion_matrix(r1,r2,img_name)
+                            if not type(new_kappa)==str:
+                                conf_mat_list.append(new_conf_mat)
+                                kappa_list.append(new_kappa)
+                
+                new_conf_mat = np.sum(np.array(conf_mat_list),axis=0)
+                new_kappa = np.mean(kappa_list)
+            else:
+                # Show example image
+                images_rater_0 = list(self.annotator_img_dict[list(self.annotator_img_dict.keys())[0]].keys())[0]
+                image_list = [self.annotator_img_dict[r][images_rater_0]['Image'] if images_rater_0 in self.annotator_img_dict[r] else np.zeros((200,200)) for r in self.annotator_img_dict]
+
+                new_img_figure = self.make_img_subplots(image_list,list(self.annotator_img_dict.keys()))
+
+                conf_mat_list = []
+                kappa_list = []
+                for r1 in raters_list_1:
+                    for r2 in raters_list_2:
+                        # Not comparing a rater to themselves
+                        if not r1==r2:
+                            # Finding overlapping images between two raters
+                            imgs_r1 = list(self.annotator_img_dict[r1].keys())
+                            imgs_r2 = list(self.annotator_img_dict[r2].keys())
+
+                            overlap_list = list(set(imgs_r1)&set(imgs_r2))
+                            for img in overlap_list:
+                                new_conf_mat, new_kappa = self.generate_new_confusion_matrix(r1,r2,img)
+                                conf_mat_list.append(new_conf_mat)
+                                kappa_list.append(new_kappa)
+                
+                new_conf_mat = np.sum(np.array(conf_mat_list),axis=0)
+                new_kappa = np.mean(kappa_list)
+        
         x_axes_labels = [rater_1+'_'+i for i in self.annotation_codes]
         y_axes_labels = [rater_2+'_'+i for i in self.annotation_codes]
 
+        if np.shape(new_conf_mat)[0]>len(x_axes_labels):
+            x_axes_labels+=[rater_1+'_Unscored']
+            y_axes_labels+=[rater_2+'_Unscored']
+
+        x_axes_labels.reverse()
+
         # Constructing the confusion matrix with labeled axes and title
         conf_mat_figure = go.Figure()
-        conf_img_data = px.imshow(new_conf_mat,x = x_axes_labels, y = y_axes_labels,text_auto=True,
-                        title=f'Kappa Score between {rater_1} and {rater_2} = {new_kappa}')
+        conf_img_data = px.imshow(np.flip(new_conf_mat,axis=1),x = x_axes_labels, y = y_axes_labels,text_auto=True)
                         
         for trace in conf_img_data['data']:
             conf_mat_figure.add_trace(trace)
-        conf_mat_figure.update_layout()
+
+        conf_mat_figure.update_layout(title=f'Kappa Score between {rater_1} and {rater_2} = {new_kappa}')
 
         return new_img_figure, conf_mat_figure
 
-    def make_img_subplots(self,image_1,image_2, rater_1, rater_2):
-        new_figure = make_subplots(rows=1,cols=2,subplot_titles = (f'Annotator 1: {rater_1}',f'Annotator 2: {rater_2}'))
-        imgs_to_include = [
-                            px.imshow(image_1),
-                            px.imshow(image_2)
-                            ]
+    def make_img_subplots(self,image_list, rater_list):
+        new_figure = make_subplots(rows=1,cols=len(image_list),subplot_titles = tuple([f'Annotator {i}: {rater_list[i]}' for i in range(len(rater_list))]))
+        imgs_to_include = [px.imshow(i) for i in image_list]
 
         for i, figure in enumerate(imgs_to_include):
             if 'data' in figure:
@@ -196,20 +285,32 @@ class CellKappaVis:
 
     def generate_new_confusion_matrix(self, rater_1, rater_2, image_name):
         
-        # In this case, the centroids are stored in a dict format so 
-        centroids_1 = self.annotator_img_dict[rater_1][image_name]['All_Cell_Types']
-        centroids_2 = self.annotator_img_dict[rater_2][image_name]['All_Cell_Types']
+        if image_name not in self.annotator_img_dict[rater_1] or image_name not in self.annotator_img_dict[rater_2]:
+            conf_mat = np.zeros((3,3))
+            kappa_score = f'{image_name} not annotated by both'
+        else:
+            # In this case, the centroids are stored in a dict format so 
+            centroids_1 = self.annotator_img_dict[rater_1][image_name]['All_Cell_Types']
+            centroids_2 = self.annotator_img_dict[rater_2][image_name]['All_Cell_Types']
 
-        # KDTree search
-        tree = cKDTree(centroids_1.values[:,0:2])
-        distances, idxes = tree.query(centroids_2.values[:,0:2])
-        include_idxes = idxes[distances<=15]
-        unscored_idxes = idxes[distances>15]
+            # KDTree search both sets of centroids for nearest points in other set of labels
+            tree_1 = cKDTree(centroids_1.values[:,0:2])
+            distances1, idxes1 = tree_1.query(centroids_2.values[:,0:2])
+            include_idxes_1 = np.unique(idxes1[distances1<=15])
 
-        overlapping_cells = pd.DataFrame({rater_1:centroids_1.iloc[include_idxes,-1],rater_2:centroids_2.iloc[include_idxes,-1]})
-        
-        conf_mat = confusion_matrix(overlapping_cells[rater_1],overlapping_cells[rater_2])
-        kappa_score = cohen_kappa_score(overlapping_cells[rater_1],overlapping_cells[rater_2])
+            tree_2 = cKDTree(centroids_2.values[:,0:2])
+            distances2, idxes2 = tree_2.query(centroids_1.values[:,0:2])
+            include_idxes_2 = np.unique(idxes2[distances2<=15])
+
+            # Now we have overlapping indices for both rater_1 and rater_2
+
+            rater_1_overlap = pd.DataFrame({rater_1:centroids_1.iloc[include_idxes_1,-1]},index=include_idxes_1)
+            rater_2_overlap = pd.DataFrame({rater_2:centroids_2.iloc[include_idxes_2,-1]},index=include_idxes_2)
+
+            overlapping_cells = pd.concat([rater_1_overlap,rater_2_overlap],axis=1).fillna('Unscored')
+
+            conf_mat = confusion_matrix(overlapping_cells.iloc[:,0],overlapping_cells.iloc[:,1])
+            kappa_score = cohen_kappa_score(overlapping_cells.iloc[:,0],overlapping_cells.iloc[:,1])
 
         return conf_mat, kappa_score
 
